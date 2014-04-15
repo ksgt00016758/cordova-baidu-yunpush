d19676492d9800c672fd570432e330264d8c2b35
bruce <hww-315@163.com>
2014-04-15 13:37:21 +0800

    fix the ios code error

--- src/ios/CDVPushPlugin.h
+++ src/ios/CDVPushPlugin.h
@@ -2,7 +2,7 @@
 #import <Cordova/CDV.h>
 #import <Cordova/CDVPlugin.h>
 
-@interface PushPlugin : CDVPlugin
+@interface CDVPushPlugin : CDVPlugin
 {
     NSDictionary *notificationMessage;
     BOOL    isInline;
