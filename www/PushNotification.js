25130fd31546139b80e386c6831dd343492e9261
树下偷懒得蚁 <hww-315@163.com>
2014-04-16 16:18:03 +0800

    Update PushNotification.js

--- www/PushNotification.js
+++ www/PushNotification.js
@@ -1,28 +1,27 @@
-cordova.define("org.apache.cordova.baiduyunpush.FGPushNotification", function(require, exports, module) { var cordova = require('cordova'),
-               exec = require('cordova/exec');
-               
-               var FGPushNotification = function() {
-               this.registered = false;
-               };
-               FGPushNotification.prototype.register = function(param)
-               {
-               exec(fastgoPushNotification.successFn, fastgoPushNotification.failureFn, 'FGPushNotification', 'register', [param]);
-               };
-               
-               FGPushNotification.prototype.successFn = function(info)
-               {
-               if(info){
-               fastgoPushNotification.registered = true;
-               cordova.fireDocumentEvent("cloudPushRegistered", info);
-               }
-               };
-               
-               FGPushNotification.prototype.failureFn = function(info)
-               {
-               fastgoPushNotification.registered = false;
-               };
-               
-               var fastgoPushNotification = new FGPushNotification();
-               
-               module.exports = fastgoPushNotification;
-               });
+exec = require('cordova/exec');
+
+var FGPushNotification = function() {
+this.registered = false;
+};
+FGPushNotification.prototype.register = function(param)
+{
+exec(fastgoPushNotification.successFn, fastgoPushNotification.failureFn, 'FGPushNotification', 'register', [param]);
+};
+
+FGPushNotification.prototype.successFn = function(info)
+{
+if(info){
+fastgoPushNotification.registered = true;
+cordova.fireDocumentEvent("cloudPushRegistered", info);
+}
+};
+
+FGPushNotification.prototype.failureFn = function(info)
+{
+fastgoPushNotification.registered = false;
+};
+
+var fastgoPushNotification = new FGPushNotification();
+
+module.exports = fastgoPushNotification;
+            
